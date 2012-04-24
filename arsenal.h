d9ec9ecbcaa6e3dbe89081737ce142d8ebdad08d
unknown <emutola@EV00235A2B3F56.eemea.ericsson.se>
2012-04-24 10:48:33 +0200

    Some commit

---
+++ arsenal.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std
+void scoreGoal () {}
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+cout<<""[Dgoalllllllllllllllllllllllllll"[D\n";
+}
---
+++ src/arsenal.cc
@@ -0,0 +1,8 @@
+#include ""[Darsena.h"
+
+int main () {}[D
+
+[D[D[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[
+[A[BscoreGoal();
+}
