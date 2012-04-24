bc67bf4ff309d597b6398ad890f9e7734821f5af
unknown <emutola@EV00235A2B3F56.eemea.ericsson.se>
2012-04-24 11:01:47 +0200

    We just changed arsenal.h

--- src/arsenal.h
+++ src/arsenal.h
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std
-void scoreGoal () {}
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
-cout<<""[Dgoalllllllllllllllllllllllllll"[D\n";
+void scoreGoal () {
+cout<<"goalllllllllllllllllllllllllll\n";
 }
